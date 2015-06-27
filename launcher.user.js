737d5a960c8476cdb02f97611b831ffb6066e2a5
nabur <erhan.buyruk@hotmail.com>
2015-06-27 04:38:52 +0300

    Update launcher.user.js

--- launcher.user.js
+++ launcher.user.js
@@ -961,7 +961,7 @@ console.log("Running Bot Launcher!");
   dArc = [],
   dText = [],
   lines = [],
-  names = ["NotReallyABot"],
+	  names = ["OTTOMAN EMPİRE"],
   originalName = names[Math.floor(Math.random() * names.length)],
   sessionScore = 0,
   serverIP = "",
